ae49ce177891f6dfc42f23673b26c5c4f299282d
Mandillah123* <official1337h4x0r@gmail.com>
2022-12-30 16:23:46 +0530

    30th Dec DSA

--- leaders.js
+++ leaders.js
@@ -1,4 +1,10 @@
 // leaders
+let navbar=document.getElementsByClassName('.nav__links');
+navbar.forEach(element => {
+    document.querySelector('#menu-btn').onclick = () =>{
+        element.toggle('active');
+    }
+});
 
 
 //end of leaders
